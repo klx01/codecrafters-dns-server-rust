b8d991c3ce853de12e206a863ea5c5074d48b65b
klx01 <69247896+klx01@users.noreply.github.com>
2024-04-30 22:16:11 +0400

    stage 1, read from socket

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
-// Uncomment this block to pass the first stage
-// use std::net::UdpSocket;
+use std::net::UdpSocket;
+use anyhow::Context;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    // let mut buf = [0; 512];
-    //
-    // loop {
-    //     match udp_socket.recv_from(&mut buf) {
-    //         Ok((size, source)) => {
-    //             println!("Received {} bytes from {}", size, source);
-    //             let response = [];
-    //             udp_socket
-    //                 .send_to(&response, source)
-    //                 .expect("Failed to send response");
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error receiving data: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
+fn main() -> anyhow::Result<()> {
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053")
+        .context("Failed to bind to address")?;
+    
+    let mut buf = [0; 512];
+    
+    loop {
+        let (size, source) = udp_socket.recv_from(&mut buf)
+            .context("failed to read from socket")?;
+        let data = &buf[..size];
+        println!("Received {size} bytes from {source} {data:?}");
+        udp_socket.send_to(data, source)
+            .context("Failed to send response")?;
+    }
 }
